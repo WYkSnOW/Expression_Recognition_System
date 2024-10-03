7c7fbf5757bf1c4cb9b454adb7d42206d2a7560c
Zhihui Chen <114270839+Czh0630@users.noreply.github.com>
2024-10-03 18:02:02 -0400

    Reference 1

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,10 +27,12 @@ st.header('Methods')
 st.header('Results and Discussion')
 
 st.header('References')
-my_list = ["A. Huang and M. J. C. Samonte, "Using Emotion Analysis, Eye tracking, and Head Movement to Monitor Student Engagement among ESL Students with Facial Recognition Algorithm (Mediapipe)," 2024 7th International Conference on Advanced Algorithms and Control Engineering (ICAACE), Shanghai, China, 2024, pp. 509-513, doi: 10.1109/ICAACE61206.2024.10548871."]
+st.markdown("""
+1. A. Huang and M. J. C. Samonte, "Using Emotion Analysis, Eye tracking, and Head Movement to Monitor Student Engagement among ESL Students with Facial Recognition Algorithm (Mediapipe)," 2024 7th International Conference on Advanced Algorithms and Control Engineering (ICAACE), Shanghai, China, 2024, pp. 509-513, doi: [10.1109/ICAACE61206.2024.10548871](https://doi.org/10.1109/ICAACE61206.2024.10548871).
+2. F. M. Sadikoğlu and M. Idle Mohamed, "Facial Expression Recognition Using CNN," 2022 International Conference on Artificial Intelligence in Everything (AIE), Lefkosa, Cyprus, 2022, pp. 95-99, doi: [10.1109/AIE57029.2022.00025](https://doi.org/10.1109/AIE57029.2022.00025).
+3. K. Wang, R. Li, and L. Zhao, "Real-time facial expressions recognition system for service robot based-on ASM and SVMs," 2010 8th World Congress on Intelligent Control and Automation, Jinan, 2010, pp. 6637-6641, doi: [10.1109/WCICA.2010.5554164](https://doi.org/10.1109/WCICA.2010.5554164).
+""")
 
-# Use st.write to display the list
-st.write(my_list)
 
 st.markdown('_**Markdown**_') # see #*
 st.caption('Balloons. Hundreds of them...')
